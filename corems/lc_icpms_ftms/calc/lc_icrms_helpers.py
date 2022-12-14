8cc5447f18e33ff3105fad777b0c9ad788bc1bef
Christian Dewey <dewey.christian@gmail.com>
2022-12-13 17:03:34 -0800

    globally available helper functions for 21T assignment; jupyter notebook for Dec. 13 2022

---
+++ corems/lc_icpms_ftms/calc/lc_icrms_helpers.py
@@ -0,0 +1,473 @@
+
+####Christian 12_13_2022 
+
+import os
+from matplotlib import style
+import pandas as pd
+import numpy as np
+
+#Generate clusters of ms features across depth.
+from sklearn.cluster import AgglomerativeClustering
+import seaborn as sns
+from matplotlib.colors import LogNorm, Normalize
+
+import warnings
+
+warnings.filterwarnings("ignore")
+import sys
+sys.path.append("./")
+from pathlib import Path
+import matplotlib.pyplot as plt
+
+from corems.mass_spectra.input import rawFileReader
+from corems.molecular_id.factory.classification import HeteroatomsClassification, Labels
+from corems.molecular_id.search.priorityAssignment import OxygenPriorityAssignment
+from corems.molecular_id.search.molecularFormulaSearch import SearchMolecularFormulas
+from corems.encapsulation.factory.parameters import MSParameters
+from corems.mass_spectrum.calc.Calibration import MzDomainCalibration
+
+
+"""
+Christian Dewey 
+December, 2022
+
+Helper functions for processing 21T data for m/z windowing project
+
+Data collected from NHMFL in November, 2022
+"""
+
+def getParser(file):
+    parser = rawFileReader.ImportMassSpectraThermoMSFileReader(file)
+    return parser
+
+
+def assign_formula(parser, interval, timerange, refmasslist=None):
+    #Function to build formula assignment lists
+    #Retrieve TIC for MS1 scans over the time range between 'timestart' and 'timestop' 
+
+    tic=parser.get_tic(ms_type='MS')[0]
+    tic_df=pd.DataFrame({'time': tic.time,'scan': tic.scans})
+
+    times=list(range(timerange[0],timerange[1],interval))
+
+    results=[]
+    
+    for timestart in times:
+
+        scans=tic_df[tic_df.time.between(timestart,timestart+interval)].scan.tolist()
+
+        mass_spectrum = parser.get_average_mass_spectrum_by_scanlist(scans)    
+        mass_spectrum.molecular_search_settings.ion_charge = 1
+
+        if refmasslist:
+            mass_spectrum.settings.min_calib_ppm_error = 10
+            mass_spectrum.settings.max_calib_ppm_error = -10
+            calfn = MzDomainCalibration(mass_spectrum, refmasslist)
+            ref_mass_list_fmt = calfn.load_ref_mass_list(refmasslist)
+
+            imzmeas, mzrefs = calfn.find_calibration_points(mass_spectrum, ref_mass_list_fmt,
+                                                        calib_ppm_error_threshold=(0, 2.0),
+                                                        calib_snr_threshold=3)
+
+            calfn.recalibrate_mass_spectrum(mass_spectrum, imzmeas, mzrefs, order=2)
+
+
+        SearchMolecularFormulas(mass_spectrum, first_hit=False).run_worker_mass_spectrum()
+
+        mass_spectrum.percentile_assigned(report_error=True)
+
+        assignments=mass_spectrum.to_dataframe()
+
+        assignments['Time']=timestart
+
+        results.append(assignments)
+    
+    results=pd.concat(results,ignore_index=True)
+
+    return(results)    
+
+
+def plot_ms(df1, start_mz, end_mz, tstart=None, df2=None,df3=None, assignment= None, ax_ms=None, lbls=None, norm=False, labs=False, colors=None):   
+    
+    if ax_ms == None:
+        _, ax = plt.subplots()
+    
+    else:
+        ax = ax_ms
+
+    if colors is not None:
+        cols = colors
+    else:
+        cols = ['C0', 'C1', 'C2']
+    mzrange= end_mz - start_mz
+
+    if tstart != None:
+        ms_t_int=df1[df1['Time'] == tstart]
+    else:
+        ms_t_int=df1
+
+    ms_df = ms_t_int[((ms_t_int['Calibrated m/z']-start_mz)<mzrange) & ((ms_t_int['Calibrated m/z']-start_mz)>0)]
+
+
+    if norm:
+
+        pltcol = 'Normalized Peak Height'
+
+    else:
+        pltcol = 'S/N'
+
+    
+    if lbls is not None:
+        labels = lbls
+    else:
+        labels = [None, None, None]
+
+ 
+    _, stemlines1, _ =ax.stem('Calibrated m/z',pltcol,data=ms_df,  markerfmt=' ', basefmt=' ', linefmt=cols[0], label = labels[0])
+    
+    if (df2 is not None) and (len(df2['Molecular Formula'])>0):
+        pltdf2 = True
+        if tstart != None:
+            ms_t_int2=df2[df2['Time'] == tstart]
+        else:
+            ms_t_int2=df2
+
+        ms_df2 = ms_t_int2[(abs(ms_t_int2['Calibrated m/z']-start_mz)<mzrange)& ((ms_t_int2['Calibrated m/z']-start_mz)>0)]
+        
+        _, stemlines2, _ =ax.stem('Calibrated m/z',pltcol,data=ms_df2,  markerfmt=' ', basefmt=' ', linefmt=cols[1], label = labels[1])
+    else:
+        pltdf2 = False 
+
+    if (df3 is not None) and (len(df3['Molecular Formula'])>0):
+        pltdf3 = True
+        if tstart != None:
+            ms_t_int3=df3[df3['Time'] == tstart]
+        else:
+            ms_t_int3=df3
+
+        ms_df3 = ms_t_int3[(abs(ms_t_int3['Calibrated m/z']-start_mz)<mzrange)& ((ms_t_int3['Calibrated m/z']-start_mz)>0)]
+
+        
+        _, stemlines3, _ =ax.stem('Calibrated m/z',pltcol,data=ms_df3,  markerfmt=' ', basefmt=' ', linefmt=cols[2], label = labels[2])
+    else:
+        pltdf3 = False
+    #if pltdf3 is True:
+    #    ax.set_ylim(0, max([maxdf1, maxdf2, maxdf3]) * 1.1)
+    #elif pltdf2 is True:
+    #    ax.set_ylim(0, max([maxdf1, maxdf2]) * 1.1)
+    #else: 
+    #    ax.set_ylim(0, maxdf1 * 1.1)
+
+    ax.set_xlim(left = start_mz - mzrange*0.1, right = start_mz + mzrange + mzrange*0.1) 
+
+    if labs:
+        for mzr,peakr,mf,er in zip(ms_df['Calibrated m/z'], ms_df[pltcol], ms_df['Molecular Formula'],  ms_df['m/z Error (ppm)']):
+
+            #if (mzr- target_mz)  == 0:
+            #    mz_text = ' m/z\n%.4f' % (mzr)
+            #else:
+            #    mz_text = r'$\Delta$' + ' m/z\n%.4f' % (mzr- target_mz)
+
+            mz_text = 'm/z %.4f\n%s\n%.3f ppm' % (mzr,mf,er)
+            ax.text(mzr, peakr + 0.02 *max(ms_df[pltcol]), mz_text, ha = 'center', fontsize = 'xx-small', weight = 'bold', color=cols[0])
+
+        if df2 is not None:
+
+            for mzr,peakr,mf, er in zip(ms_df2['Calibrated m/z'], ms_df2[pltcol], ms_df2['Molecular Formula'], ms_df2['m/z Error (ppm)']):
+
+            #if (mzr- target_mz)  == 0:
+            #    mz_text = ' m/z\n%.4f' % (mzr)
+            #else:
+            #    mz_text = r'$\Delta$' + ' m/z\n%.4f' % (mzr- target_mz)
+
+                mz_text = 'm/z %.4f\n%s\n%.3f ppm' % (mzr,mf,er)
+                ax.text(mzr, peakr + 0.02 *max(ms_df2[pltcol]), mz_text, ha = 'center', fontsize = 'xx-small', weight = 'bold', color = cols[1])
+
+        if df3 is not None:
+
+            for mzr,peakr,mf, er in zip(ms_df3['Calibrated m/z'], ms_df3[pltcol], ms_df3['Molecular Formula'], ms_df3['m/z Error (ppm)']):
+
+            #if (mzr- target_mz)  == 0:
+            #    mz_text = ' m/z\n%.4f' % (mzr)
+            #else:
+            #    mz_text = r'$\Delta$' + ' m/z\n%.4f' % (mzr- target_mz)
+
+                mz_text = 'm/z %.4f\n%s\n%.3f ppm' % (mzr,mf,er)
+                ax.text(mzr, peakr + 0.02 *max(ms_df3[pltcol]), mz_text, ha = 'center', fontsize = 'xx-small', weight = 'bold', color = cols[2])
+
+   # theor_mz=pattern.mdiff+result['mass']
+   # theor_int=pattern.ratio*result['abundance']
+   # ax.stem(theor_mz,theor_int, basefmt=' ',linefmt='gray')
+
+   # for isotope in pattern.isotope[pattern.requirement=='Y']:
+   #     ax.stem('mz','intense',data=result[isotope],  markerfmt=' ', basefmt=' ',linefmt='red')
+    ax.set_xlim(start_mz, end_mz)
+    
+    if ax == None:
+
+        ax.legend(('other', 'target'),bbox_to_anchor=(1.05, 1.0), loc='upper left',frameon=False)
+
+        if(assignment):
+
+            mf = assignment[0]
+            score = assignment[1]
+            er = assignment[2]
+
+            ax.text(1.05,0.7,mf,transform=ax.transAxes)
+            ax.text(1.05,0.6,'Error (ppm) = %.3f ' %er ,transform=ax.transAxes)
+            ax.text(1.05,0.5,'Score = %.3f' %score ,transform=ax.transAxes)
+
+    if norm: 
+        ax.set(xlabel='Calibrated m/z',ylabel='Normalized Intensity')
+    else: 
+        ax.set(xlabel='Calibrated m/z',ylabel='Intensity')
+    #ax.set_title('%.2f' %timerange[0] + ' to %.2f' %timerange[1] +' min', fontsize = 'medium')
+    ax.legend(bbox_to_anchor = (1.00, 0.5), frameon =False, loc = 'center left')
+    ax.axhline(y=0.0, color='black')
+    plt.setp(stemlines1,'color', cols[0], 'linewidth', 2)
+    if pltdf2 is True:
+        plt.setp(stemlines2, 'color', cols[1],'linewidth', 2, 'linestyle', '--')
+    if pltdf3 is True:
+        plt.setp(stemlines3, 'color', cols[2],'linewidth', 2)
+    plt.tight_layout()
+    if ax_ms != None:
+        return ax
+
+
+def filterMzRange(results, mz_range):
+
+    mz_i = mz_range[0]
+    mz_f = mz_range[1]
+
+    sub = results[(results['m/z'] >= mz_i) & (results['m/z'] <= mz_f)]
+
+    return sub
+
+
+def pltMZerror(results, bins=50):
+    ## assignment error distribution
+    _, ax = plt.subplots()
+    for mol_class in sorted(results['mol_class'].unique()):
+
+        counts, bins = np.histogram(np.asarray(results[results['mol_class']==mol_class]['m/z Error (ppm)']),bins = bins)
+
+        ax.plot(bins[:-1], counts, label = mol_class)
+
+    ax.set_xlim(-0.5,0.5)
+    ax.legend(frameon=False)
+    ax.set_xlabel('m/z assignment error (ppm)')
+    ax.set_ylabel('# assigned features')
+
+    return ax
+
+
+def pltMZerror_pts(results):
+    ## assignment error distribution
+    _, ax = plt.subplots()
+    for mol_class in sorted(results['mol_class'].unique()):
+
+        ax.scatter(results[results['mol_class']==mol_class]['Calibrated m/z'], results[results['mol_class']==mol_class]['m/z Error (ppm)'], label = mol_class)
+
+    ax.set_xlim(200,1200)
+    ax.legend(frameon=False,bbox_to_anchor=(1.0, 0.5))
+    ax.set_xlabel('calibrated m/z')
+    ax.set_ylabel('assignment error (ppm)')
+
+    return ax
+
+
+def assignMolClass(resultsdf, mz_cutoff = 800, sn_lim = 3):
+
+    filtered_results=resultsdf[(resultsdf['m/z']<mz_cutoff) & (resultsdf['S/N']>sn_lim)]
+
+    filtered_results['N']=filtered_results['N'].fillna(0)
+    filtered_results['O']=filtered_results['O'].fillna(0)
+    #filtered_results['S']=filtered_results['S'].fillna(0)
+    #filtered_results['P']=filtered_results['Fe'].fillna(0)
+    filtered_results['Cu']=filtered_results['Cu'].fillna(0)
+
+    filtered_results['mol_class']='Unassigned'
+    filtered_results['mol_class'][filtered_results['C']>0]='CHO'
+    filtered_results['mol_class'][(filtered_results['C']>0) & (filtered_results['N']>0.5)]='CHON'
+    #filtered_results['mol_class'][(filtered_results['C']>0) & (filtered_results['S']>0.5)]='CHOS'
+    #filtered_results['mol_class'][(filtered_results['C']>0) & (filtered_results['Fe']>0.5)]='CHOFe'
+    filtered_results['mol_class'][(filtered_results['C']>0) & (filtered_results['Cu']>0.5)]='CHOCu'
+    filtered_results['mol_class'][(filtered_results['C']>0) & (filtered_results['Cu']>0.5) & (filtered_results['N']>0.5)]='CHONCu'
+    #filtered_results['mol_class'][(filtered_results['C']>0) & (filtered_results['Cu']>0.5) & (filtered_results['S']>0.5)]='CHOSCu'
+    #filtered_results['mol_class'][(filtered_results['C']>0) & (filtered_results['Cu']>0.5) & (filtered_results['N']>0.5) & (filtered_results['S']>0.5)] ='CHONSCu'
+    #filtered_results['mol_class'][(filtered_results['C']>0) & (filtered_results['Na']>0.5) & (filtered_results['N']>0.5)]='CHONNa'
+    #filtered_results['mol_class'][(filtered_results['C']>0) & (filtered_results['P']>0.5) & (filtered_results['Na']>0.5) & (filtered_results['N']>0.5)]='CHONPNa'
+
+
+    results= filtered_results #[filtered_results['mol_class'] != 'Unassigned'] #[filtered_results['Is Isotopologue']==0]
+    results['N/C']=results['O']/results['C']
+    results['H/C']=results['H']/results['C']
+    results['N/C']=results['N']/results['C']
+
+    return results
+
+
+def add_mzwindow_col(df):    
+
+    df['m/z window'] = df.index
+    
+    
+    for file, r in zip(df['file'], range(len(df['file']))):
+
+        if ('400_500' in file) or ('400-500' in file):
+
+            df['m/z window'].iloc[r] = '400-500 m/z'
+
+        elif ('500_600' in file) or ('500-600' in file):
+
+            df['m/z window'].iloc[r] = '500-600 m/z'
+    
+        elif ('600_700' in file) or ('600-700' in file):
+
+            df['m/z window'].iloc[r] = '600-700 m/z'
+
+        elif ('700_800' in file) or ('700-800' in file):
+
+            df['m/z window'].iloc[r] = '700-800 m/z'
+
+        elif ('400_600' in file) or ('400-600' in file):
+
+            df['m/z window'].iloc[r] = '400-600 m/z'
+        
+        elif ('600_800' in file) or ('600-800' in file):
+
+            df['m/z window'].iloc[r] = '600-800 m/z'
+            
+        elif 'full' in file:
+
+            df['m/z window'].iloc[r] = '200-1200 m/z'
+
+    return df 
+
+
+def getUniqueFeatures(df):    
+    #Create a list of all unique features and describe their intensity. 
+    print('total # results: %s' %len(df))
+    #define a list of unique features (time, formula) with 'areas' determined for each sample. There may be a slight bug that causes the unique list to grow...
+    uniquelist=[]
+    for time in df.Time.unique():
+        current=df[df.Time==time]
+        current=current.sort_values(by=['m/z Error (ppm)'],ascending=True)
+        currentunique=current.drop_duplicates(subset=['Molecular Formula'])
+        currentunique=currentunique[currentunique['C']>1]
+        currentunique=currentunique.set_index(['Molecular Formula'],drop=False)
+        for file in df['file'].unique():
+            current_file=current[current['file']==file].drop_duplicates(subset=['Molecular Formula'])
+            current_file=current_file.rename(columns={'Peak Height':file})
+            current_file=current_file.set_index(['Molecular Formula'],drop=False)
+            #current_file[file].fillna(0)
+            currentunique=currentunique.join(current_file[file])
+        uniquelist.append(currentunique)
+
+    unique_results=pd.concat(uniquelist,ignore_index=True)
+    unique_results['N/C']=unique_results['N']/unique_results['C']
+    #unique_results['blank']=unique_results[blankfile]/unique_results['Peak Height']
+    #unique_results=unique_results[unique_results['blank']<blnk_thresh]
+
+
+    print('# unique results: %s' %len(unique_results))
+
+    return unique_results
+
+
+
+def plotUnique(df,ps=50,includeBlanks=False, xlim = [400,600]):
+    xmin = xlim[0]
+    xmax = xlim[1]
+    if includeBlanks != True:
+        mask = ~df['file'].str.contains('qh2o', case=False, na=False)
+        df=df[mask]
+    else:
+        df=df
+    fig, ((ax2, ax4),(ax3, ax1)) = plt.subplots(2,2,figsize = (12,12))
+    sns.scatterplot(x='m/z',y='m/z Error (ppm)',data=df[df['Cu']>0],hue='m/z window', s=ps*4, ax=ax1)
+    sns.scatterplot(x='m/z',y='m/z Error (ppm)', hue='m/z window', data=df, ax=ax2,s=ps)
+    ax1.set_xlim(xmin,xmax)
+    ax2.set_xlim(xmin,xmax)
+    sns.scatterplot(x='m/z',y='m/z Error (ppm)',hue='N',data=df,ax=ax3,s=ps)
+    sns.scatterplot(x='m/z',y='Resolving Power',hue='mol_class',data=df,ax=ax4)
+    
+    ax1.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    ax1.set_title('Assignment error, Cu features')
+    ax2.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    ax2.set_title('Overall assignment error')
+    ax3.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    ax3.set_title('Assignment error, N features')
+    ax4.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    ax4.set_title('Resolving power v. m/z')
+
+    #fig.savefig('unique_results.pdf', bbox_to_inches='tight')
+
+    return fig
+
+
+def addRepCol(data_df):
+
+    data_df['Rep'] = data_df.index
+
+
+    for file in data_df['file'].unique():
+
+        print(file)
+
+        if 'rep2' in file:
+
+            temp = data_df[data_df['file'] == file]
+            temp['Rep'] = 2
+            data_df[data_df['file'] == file] = temp
+
+
+        else:
+
+            temp = data_df[data_df['file'] == file]
+            temp['Rep'] = 1
+            data_df[data_df['file'] == file] = temp
+
+    print(data_df['Rep'].unique())
+
+
+    return data_df 
+
+
+def blankSubtract(df, blnkthresh = 0.9):
+    
+    holder = []
+    for file in df['file'].unique():
+        
+        sub = df[df['file'] == file]
+
+        blkf = sub['blank file'].iloc[0]
+
+
+        sub[sub[file]== np.nan] = 0
+
+        nom = sub[file]
+        den = sub[blkf]
+
+        nom = nom.replace(np.nan,0)
+        den = den.replace(np.nan,1)
+
+        if file != blkf:
+            nom = nom
+        elif file == blkf:
+            nom = nom * (blnkthresh*0.8)
+
+        sub['blank subtract'] = nom/den
+        holder.append(sub)
+    df_end = pd.concat(holder)
+    df_end = df_end[df_end['blank subtract'] > blnkthresh]
+    return df_end
+
+
+def normMS(df,fulldf):
+
+    max_i = max(fulldf['Peak Height'].values)
+
+    df['Normalized Peak Height'] = df['Peak Height'] / max_i
+
+    return df
