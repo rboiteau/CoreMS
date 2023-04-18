7eb448ad0d62948ad4a9ed6263b66533328c2368
dewey-air <christiandewey@Christians-MacBook-Air.local>
2023-04-18 16:06:31 -0500

    print cmd line 105 mass_spectrum.output.export.py only print Ion Charge col

--- corems/mass_spectrum/output/export.py
+++ corems/mass_spectrum/output/export.py
@@ -102,7 +102,7 @@ class HighResMassSpecExport(Thread):
         columns = self.columns_label + self.get_all_used_atoms_in_order(self.mass_spectrum)
         dict_data_list = self.get_list_dict_data(self.mass_spectrum)
         df = DataFrame(dict_data_list, columns=columns)
-        print(df['Ion Charge','Molecular Formula'])
+        print(df['Ion Charge'])
         df.name = self.output_file
         return df
 
