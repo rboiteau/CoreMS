7af8e9244c3c9d9e40241d62e3effd556261e42a
dewey-air <christiandewey@Christians-MacBook-Air.local>
2023-04-18 17:06:12 -0500

    changed line 207, mass_spectrum.calc.PeakPicking.py, ion_charge = self.polarity * MSParameters.molecular_search.ion_charge; changed line 980, mass_spectrum.factory.MassSpectrumClasses.py: ion_charge = self.polarity * MSParameters.molecular_search.ion_charge

--- corems/mass_spectrum/calc/PeakPicking.py
+++ corems/mass_spectrum/calc/PeakPicking.py
@@ -8,6 +8,7 @@ from numpy import hstack, inf, isnan, poly1d, polyfit, where, array
 from numpy import float as npfloat
 from corems.encapsulation.constant import Labels
 from corems.mass_spectra.calc import SignalProcessing as sp
+from corems.encapsulation.factory.parameters import MSParameters
 
 class PeakPicking:
 
@@ -203,7 +204,7 @@ class PeakPicking:
                     
                     peak_resolving_power = self.calculate_resolving_power( abund, mass, apex_index)
                     s2n = intes_centr/self.baselise_noise_std
-                    ion_charge = self.polarity * self._parameters.molecular_search.ion_charge
+                    ion_charge = self.polarity * MSParameters.molecular_search.ion_charge
                     print('ion charge %s ' %ion_charge)
                     self.add_mspeak(ion_charge, mz_exp_centroid, abund[apex_index] , peak_resolving_power, s2n, indexes_tuple, exp_freq=freq_centr, ms_parent=self)
                 #pyplot.plot(domain[start_index: final_index + 1], signal[start_index:final_index + 1], c='black')
