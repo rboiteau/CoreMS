a7bbc2c2365f29de89380c817c488bd2c7dc8d1f
dewey-air <christiandewey@Christians-MacBook-Air.local>
2023-04-21 11:10:23 -0500

    updated assign script

--- 230421_spring_pos_21T_assign.py
+++ 230421_spring_pos_21T_assign.py
@@ -61,21 +61,16 @@ def assign_formula(esifile, times, cal_ppm_threshold=(-1,1), refmasslist=None):
     tic=parser.get_tic(ms_type='MS')[0]
     tic_df=pd.DataFrame({'time': tic.time,'scan': tic.scans})
 
-    results_1 = []
-    results_2 = []
+    results = []
     
     for timestart in times:
         print('\nfile: %s\ntimestart:%s'  %(esifile,timestart))
     
         scans=tic_df[tic_df.time.between(timestart,timestart+interval)].scan.tolist()
-        print('\nassigning with first parameter set...')
-
         
         mass_spectrum = parser.get_average_mass_spectrum_by_scanlist(scans)  
 
-        setAssingmentParams(ion_charge = 1)
-
-        #print("MS Obj loaded - "+str(len(mass_spectrum.mspeaks))+" peaks found.")
+        setAssingmentParams()
 
         if refmasslist:
         
@@ -100,38 +95,19 @@ def assign_formula(esifile, times, cal_ppm_threshold=(-1,1), refmasslist=None):
 
         mass_spectrum.percentile_assigned(report_error=True)
 
-        assignments_z1=mass_spectrum.to_dataframe()
+        assignments=mass_spectrum.to_dataframe()
 
-        assignments_z1['Time']=timestart
+        assignments['Time']=timestart
 
-        results_1.append(assignments_z1)
-
-
-        print('\nassigning with second parameter set...')
-
-        mass_spectrum.molecular_search_settings.ion_charge = 2
-        
-        setAssingmentParams2(ion_charge =2 )
-
-        SearchMolecularFormulas(mass_spectrum, first_hit=True).run_worker_mass_spectrum()
-
-        print('\nresults with second parameter set...')
-        
-        mass_spectrum.percentile_assigned(report_error=True)
-
-        assignments_z2=mass_spectrum.to_dataframe()
-
-        assignments_z2['Time']=timestart
-
-        results_2.append(assignments_z2)
-
-    results_1=pd.concat(results_1,ignore_index=True)
-    results_2=pd.concat(results_2,ignore_index=True)
-
-    return(results_1, results_2)   
+        results.append(assignments)
 
 
-def setAssingmentParams(ion_charge):
+    results=pd.concat(results,ignore_index=True)
+
+    return(results)   
+
+
+def setAssingmentParams():
     # set assignment parameters
     MSParameters.molecular_search.error_method = 'None'
     MSParameters.molecular_search.min_ppm_error = -0.25
@@ -147,7 +123,7 @@ def setAssingmentParams(ion_charge):
     MSParameters.molecular_search.url_database = 'postgresql+psycopg2://coremsappdb:coremsapppnnl@localhost:5432/coremsapp'
     MSParameters.molecular_search.min_dbe = -1
     MSParameters.molecular_search.max_dbe = 20
-    MSParameters.molecular_search.ion_charge = ion_charge # absolute value; multiplied by polarity w/in code
+    #MSParameters.molecular_search.ion_charge = ion_charge # absolute value; multiplied by polarity w/in code
 
     MSParameters.molecular_search.usedAtoms['C'] = (1,50)  
     MSParameters.molecular_search.usedAtoms['H'] = (4,100)
@@ -162,35 +138,6 @@ def setAssingmentParams(ion_charge):
     #MSParameters.molecular_search.usedAtoms['Zn'] = (0,1)
 
 
-def setAssingmentParams2(ion_charge):
-    # set assignment parameters
-    MSParameters.molecular_search.error_method = 'None'
-    MSParameters.molecular_search.min_ppm_error = -0.25
-    MSParameters.molecular_search.max_ppm_error = 0.25
-
-    MSParameters.molecular_search.isProtonated = True
-    MSParameters.molecular_search.isRadical = False
-    MSParameters.molecular_search.isAdduct = False
-
-    MSParameters.molecular_search.score_method = "prob_score"
-    MSParameters.molecular_search.output_score_method = "prob_score"
-
-    MSParameters.molecular_search.url_database = 'postgresql+psycopg2://coremsappdb:coremsapppnnl@localhost:5432/coremsapp'
-    MSParameters.molecular_search.min_dbe = -1
-    MSParameters.molecular_search.max_dbe = 40
-    MSParameters.molecular_search.ion_charge = ion_charge # absolute value; multiplied by polarity w/in code
-
-    MSParameters.molecular_search.usedAtoms['C'] = (10,100)  
-    MSParameters.molecular_search.usedAtoms['H'] = (10,150)
-    MSParameters.molecular_search.usedAtoms['O'] = (0,20)
-    MSParameters.molecular_search.usedAtoms['N'] = (0,10)
-    MSParameters.molecular_search.usedAtoms['S'] = (0,0)
-    MSParameters.molecular_search.usedAtoms['P'] = (0,0)
-    MSParameters.molecular_search.usedAtoms['Na'] = (0,0)
-    #MSParameters.molecular_search.usedAtoms['Cu'] = (0,1)
-    #MSParameters.molecular_search.usedAtoms['K'] = (0,1)
-    #MSParameters.molecular_search.usedAtoms['Fe'] = (0,1)
-    #MSParameters.molecular_search.usedAtoms['Zn'] = (0,1)
 
 
 if __name__ == '__main__':
@@ -200,16 +147,14 @@ if __name__ == '__main__':
     
     data_dir = '/mnt/disks/orca-data/mz-windowing/pos/spring/'
 
-    fname1 = '230420_spring-env_pos_+1a.csv'
-    fname2 = '230420_spring-env_pos_+2a.csv'
+    fname = '230421_spring-env_pos.csv'
 
     mzref = "/home/CoreMS/tests/tests_data/ftms/nom_pos.ref" 
     
     interval = 4     # window in which scans are averaged
     time_range = [8,12]    
 
-    results1 = []
-    results2 = []
+    results = []
 
     times = list(range(time_range[0],time_range[1],interval))
 
@@ -221,18 +166,13 @@ if __name__ == '__main__':
     for f in f_raw:
         if 'spring_fullmz_rep2' in f:
             print("\n\n\n%s/%s files" %(i, len(f_raw)))
-            output1, output2 = assign_formula(esifile = f, times = times, cal_ppm_threshold=(-1,1), refmasslist = mzref)
-            output1['file'] = f 
-            output2['file'] = f 
-            results1.append(output1)
-            results2.append(output2)
+            output = assign_formula(esifile = f, times = times, cal_ppm_threshold=(-1,1), refmasslist = mzref)
+            output['file'] = f 
+            results.append(output)
             i = i + 1
     
-    df1 = pd.concat(results1)
-    df1.to_csv(data_dir+fname1)
-
-    df2 = pd.concat(results2)
-    df2.to_csv(data_dir+fname2)
+    df = pd.concat(results)
+    df.to_csv(data_dir+fname)
 
     printRunTime()
 
